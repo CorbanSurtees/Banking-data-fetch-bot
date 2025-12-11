d9f888dc014c915915d7251f1935ea747205201c
Corban Surtees <corban.surtees@magiqsoftware.com>
2025-12-12 10:58:45 +1300

    sdgswhrgdsasf

--- BK/bin/update_banks.rb
+++ BK/bin/update_banks.rb
@@ -54,6 +54,7 @@ def latest_au_bsb_directory
   today = Date.today.strftime("%Y%m%d")
   dest = File.join(SQL_DIR, "BSBDirectory.csv")
   download(url, dest)
+  convert_csv_to_txt_dos(dest, dest)
 end
 
 def latest_timezone_data
